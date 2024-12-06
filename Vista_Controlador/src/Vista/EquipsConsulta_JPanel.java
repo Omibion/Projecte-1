89af2838558675ede7871c0035e50ac32c36ca69
Omibion <davidholgadoberrocal@gmail.com>
2024-12-06 16:37:43 +0100

    ara si que si que he acabao el jpanel, que me habia dejado el boton de eliminar

--- Vista_Controlador/src/Vista/EquipsConsulta_JPanel.java
+++ Vista_Controlador/src/Vista/EquipsConsulta_JPanel.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Vista;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
@@ -12,33 +9,38 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 /**
- *
- * @author isard
+ * Panel de consulta de equipos.
  */
-public class EquipsConsulta_JPanel extends JPanel{
-    
+public class EquipsConsulta_JPanel extends JPanel {
+
     JTable taulaEquips;
     JButton CreaEquips;
-    JButton MostraEquip;
-   JComboBox Temporada;
-   JComboBox Categoria;
+    JButton EditaEquip;
+    JButton BorraEquip;
+    JComboBox Temporada;
+    JComboBox Categoria;
 
     public EquipsConsulta_JPanel() {
        
         setLayout(new BorderLayout());
-        taulaEquips = new JTable(5, 3); 
+
+    
+        taulaEquips = new JTable(5, 3);
         CreaEquips = new JButton("Crear Equip");
-        MostraEquip = new JButton("Mostrar Equip");
+        EditaEquip = new JButton("Editar Equip");
+        BorraEquip = new JButton("Borrar Equip");
+        
         add(new JScrollPane(taulaEquips), BorderLayout.CENTER);
-        JPanel bottomPanel = new JPanel(new BorderLayout());
-        bottomPanel.add(CreaEquips, BorderLayout.WEST);
-        bottomPanel.add(MostraEquip, BorderLayout.EAST);
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 5));
+        bottomPanel.add(CreaEquips); 
+        bottomPanel.add(EditaEquip); 
+        bottomPanel.add(BorraEquip); 
         add(bottomPanel, BorderLayout.SOUTH);
         JPanel topPanel = new JPanel(new BorderLayout());
         topPanel.add(Temporada, BorderLayout.WEST);
         topPanel.add(Categoria, BorderLayout.EAST);
         add(topPanel, BorderLayout.NORTH);
     }
-   
+
    
 }
